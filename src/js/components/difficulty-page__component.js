d8554a3bfc860ce80c1c14030c60d941bb105d1a
Julia Matsievich <juliamatsievich@mail.ru>
2023-07-05 23:35:01 +0500

    добавляет поле карт в зависимости от сложности

--- src/js/components/difficulty-page__component.js
+++ src/js/components/difficulty-page__component.js
@@ -5,26 +5,27 @@ let difficultValue;
 
 export const renderChangeDifficultyPage = (appEl) => {
    const changeDifficultyHtml = `
-	<div class="popup">
-	<div class="popup__container">
-		<div class="popup__title">Выбери сложность</div>
-		<div class="popup__content popup__content-difficulty">
-			<div class="difficulty__container">
-				<div class="difficulty__block">
-					<input id="difficult-1" type="radio" name="radio" value="1" class="difficulty__input">1
-				</div>
-				<div class="difficulty__block">
-					<input id="difficult-2" type="radio" name="radio" value="2" class="difficulty__input">2
-				</div>
-				<div class="difficulty__block">
-					<input id="difficult-3" type="radio" name="radio" value="3" class="difficulty__input">3
-				</div>
-			</div>
-		</div>
-		<button class="button start__button">Старт</button>
-	</div>
-
-</div>
+      <div class="app__popup">
+         <div class="popup">
+            <div class="popup__container">
+               <div class="popup__title">Выбери сложность</div>
+                  <div class="popup__content popup__content-difficulty">
+                     <div class="difficulty__container">
+                        <div class="difficulty__block">
+                           <input id="difficult-1" type="radio" name="radio" value="1" class="difficulty__input">1
+                        </div>
+                     <div class="difficulty__block">
+                     <input id="difficult-2" type="radio" name="radio" value="2" class="difficulty__input">2
+                  </div>
+                  <div class="difficulty__block">
+                     <input id="difficult-3" type="radio" name="radio" value="3" class="difficulty__input">3
+                  </div>
+               </div>
+            </div>
+            <button class="button start__button">Старт</button>
+            </div>
+         </div>
+      </div>
 	`;
    appEl.innerHTML = changeDifficultyHtml;
 
--- src/js/components/game-page__component.js
+++ src/js/components/game-page__component.js
@@ -1,25 +1,64 @@
-export const renderGamePage = (appEl, difficultValue) => {
-   if (difficultValue === "1") {
-      const gameDifficulty1Html = `
-		<div>Это первый уровень сложности</div>
-		`;
+const getGameField = (gameBlock, qtyCard) => {
+   const gameCardHtml = `
+   <div class="game__card">
+      <img src="./src/img/shirt.png" alt="">
+   </div>
+`;
+   let gameDifficultyarr = [];
 
-      appEl.innerHTML = gameDifficulty1Html;
+   for (let i = 1; i <= qtyCard; i++) {
+      gameDifficultyarr.push(gameCardHtml);
+   }
+
+   const gameDifficultyHtml = gameDifficultyarr.join("");
+
+   gameBlock.innerHTML = gameDifficultyHtml;
+};
+
+export const renderGamePage = (appEl, difficultValue) => {
+   const gameHtml = `
+   <div class="app__game">
+   <div class="header">
+      <div class="header__time">
+         <span class="time__min">00</span>
+         <span class="time__point">.</span>
+         <span class="time__sek">00</span>
+      </div>
+      <div class="header__button">
+         <button class="button button__start-again">Начать заново</button>
+      </div>
+   </div>
+   <div class="game">
+
+   </div>
+</div>
+   `;
+
+   appEl.innerHTML = gameHtml;
+
+   const gameBlock = document.querySelector(".game");
+
+   const gameCardHtml = `
+      <div class="game__card">
+         <img src="./src/img/shirt.png" alt="">
+      </div>
+   `;
+
+   if (difficultValue === "1") {
+      let qtyCard = 12;
+      getGameField(gameBlock, qtyCard);
+      gameBlock.classList.add("game__difficult_1");
    }
 
    if (difficultValue === "2") {
-      const gameDifficulty2Html = `
-		<div>Это второй уровень сложности</div>
-		`;
-
-      appEl.innerHTML = gameDifficulty2Html;
+      let qtyCard = 18;
+      getGameField(gameBlock, qtyCard);
+      gameBlock.classList.add("game__difficult_2");
    }
 
    if (difficultValue === "3") {
-      const gameDifficulty3Html = `
-		<div>Это третий уровень сложности</div>
-		`;
-
-      appEl.innerHTML = gameDifficulty3Html;
+      let qtyCard = 36;
+      getGameField(gameBlock, qtyCard);
+      gameBlock.classList.add("game__difficult_3");
    }
 };
